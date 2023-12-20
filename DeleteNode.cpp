ce951912405b45cff71e97dd816cdb358921a0f4
pnkjshahare <pankajsahare.04@gmail.com>
2023-12-20 14:32:56 +0530

    done

---
+++ DeleteNode.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+    int data;
+    Node *next;
+    Node(int data, Node *next)
+    {
+        data = data;
+        next = next;
+    }
+    Node(int data)
+    {
+        data = data;
+        next = nullptr;
+    }
+};
+int main()
+{
+
+    return 0;
+}
